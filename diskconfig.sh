d8ae18634c25a9cf7cedeb49c73ce1c3a3867ee8
root user <root@domain.com>
2024-02-26 13:52:07 -0500

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -11,3 +11,5 @@ fdisk -l | head -17 >>$FILENAME.report
  
 echo -e "\n\n Mounted Filesystems: \n\n" >>$FILENAME.report
 df -hT | grep -v tmp >>$FILENAME.report
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
